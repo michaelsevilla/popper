be72e535c03e522042e286a12b9a89390a380b79
Ivo Jimenez <ivo.jimenez@gmail.com>
2017-01-19 11:45:42 -0800

    Fixes provisioning of .deb packages

--- templates/experiments/linux-cgroups/run.sh
+++ templates/experiments/linux-cgroups/run.sh
@@ -2,14 +2,7 @@
 set -e -x
 
 # build deb packages
-rm -f vagrant/debs/*
-docker build -t kernel-ci docker/
-docker run --rm -ti \
-  -e CHECK_KEY=false \
-  -v `pwd`/docker/linux:/linux \
-  -v `pwd`/patches:/app/patches \
-  kernel-ci
-cp docker/linux/*.deb vagrant/debs/
+./build-kernel.sh
 
 # provision new kernel and start VM
 (cd vagrant && ./provision.sh)
--- templates/experiments/linux-cgroups/vagrant/provision.sh
+++ templates/experiments/linux-cgroups/vagrant/provision.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e -x
 
+# check if vagrant can run OK
+vagrant status
+
 # bring VM up if it has never been created
 vmexists=`vagrant status | grep 'not created' | wc -l`
-if [ $vmexists -ne 1 ]; then
+if [ $vmexists -ne 0 ]; then
   vagrant up
 fi
 
